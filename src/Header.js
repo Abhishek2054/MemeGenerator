5bca9060d2aa10d87e623f405d97199fa0a829b2
Adrinlol <l.qqbadze@gmail.com>
2019-05-17 13:17:22 +0400

    MemeGenerator

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                src="http://www.pngall.com/wp-content/uploads/2016/03/Sad-Batman-Ben-Affleck-Meme-PNG.png" 
+                alt="The Unbearable Sadness"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
